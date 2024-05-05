b56a39980e0dfc5c1f6e0919b6382b66c75f3d56
Talha Ameer <talhaameer713@gmail.com>
2024-05-05 16:15:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBd69UI9bAna_NVO9LnQUvg8ibOy93Yv28",
+    authDomain: "fir-class1-424c0.firebaseapp.com",
+    projectId: "fir-class1-424c0",
+    storageBucket: "fir-class1-424c0.appspot.com",
+    messagingSenderId: "643394619425",
+    appId: "1:643394619425:web:8638ad5808bd39405bfb60",
+    measurementId: "G-QE5RQX7YKW"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+let getBtn = document.querySelector("#btn")
+getBtn.addEventListener('click', async () => {
+
+    let name = document.querySelector("#name")
+    let fname = document.querySelector("#fname")
+    let phone = document.querySelector("#phone")
+
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            name: name.value,
+            fname: fname.value,
+            phone: parseInt(phone.value) 
+        });
+        console.log("Document written with ID: ", docRef.id);
+        console.log("Document ==>", docRef);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+
+
+
+})
